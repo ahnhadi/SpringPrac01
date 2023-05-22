22c78d1d6ff933b0ca951604b216a6783b0479fe
haeri <hadi43@naver.com>
2023-05-22 17:05:29 +0900

    #2 [feat] 블로그 CRUD 기능을 thymeleaf를 통하여 View와 연결

--- src/main/java/me/haeri/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/haeri/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -2,7 +2,9 @@ package me.haeri.springbootdeveloper;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // 생성시간, 수정시간 자동 업데이트
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
   public static void main(String[] args) {
---
+++ src/main/java/me/haeri/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,23 @@
+package me.haeri.springbootdeveloper.dto;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.haeri.springbootdeveloper.entity.Article;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+  private Long id;
+  private String title;
+  private String content;
+  private LocalDateTime createdAt;
+
+  public ArticleViewResponse(Article article){
+    this.id = article.getId();
+    this.title = article.getTitle();
+    this.content = article.getContent();
+    this.createdAt = article.getCreatedAt();
+  }
+}
